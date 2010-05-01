73de91681795b42ae4095413f059d201f26d6153
Stephen Lombardi <stephen.a.lombardi@gmail.com>
2010-05-01 09:40:51 -0400

    changed the test file for planarvolume, also make a makefile for the interpreter

---
+++ examples/test.sh
@@ -0,0 +1,17 @@
+cd testkdtree
+./test
+cd ..
+
+cd testmatrix
+./test
+cd ..
+
+cd testparsemap
+./test
+cd ..
+
+cd testplanarvolume
+./test
+cd ..
+
+echo "tests passed"
--- examples/testplanarvolume/test.cpp
+++ examples/testplanarvolume/test.cpp
@@ -15,6 +15,16 @@ namespace slib {
 		}
 		return out;
 	}
+
+	template< int dim, class FieldT >
+	bool operator==( const Matrix< dim, 1, FieldT > & lhs, const Matrix< dim, 1, FieldT > & rhs ) {
+		for( int i = 0; i < dim; i++ ) {
+			if( lhs[ i ] != rhs[ i ] ) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 using namespace slib;
@@ -33,9 +43,29 @@ void printIntersectionLine( const PlanarVolume< dim, FieldT, PlaneT > & pv, cons
 }
 
 template< int dim, class FieldT, class PlaneT, template< int, class > class OffsetCalcT >
+void checkIntersectionLine( const PlanarVolume< dim, FieldT, PlaneT > & pv, const vec3f & start, const vec3f & end, const OffsetCalcT< dim, FieldT > & offsetcalc, FieldT startdist, const Matrix< dim, 1, FieldT > & startnormal, FieldT enddist, const Matrix< dim, 1, FieldT > & endnormal ) {
+	PlanarVolumeIntersection< dim, FieldT, PlaneT > pvi = pv.IntersectLine( start, end, offsetcalc );
+	//std::cout << start << " to " << end << ( pv.Within( start ) ? ": starts inside " : ": starts outside " ) << pvi << std::endl;
+	assert( pvi.enterfraction == startdist );
+	assert( !pvi.enterplane && !Norm( startnormal ) || pvi.enterplane && pvi.enterplane->GetNormal( ) == startnormal );
+	assert( pvi.exitfraction == enddist );
+	assert( !pvi.exitplane && !Norm( endnormal ) || pvi.exitplane && pvi.exitplane->GetNormal( ) == endnormal );
+}
+
+template< int dim, class FieldT, class PlaneT, template< int, class > class OffsetCalcT >
 void printIntersectionRay( const PlanarVolume< dim, FieldT, PlaneT > & pv, const vec3f & start, const vec3f & dir, const OffsetCalcT< dim, FieldT > & offsetcalc ) {
 	PlanarVolumeIntersection< dim, FieldT, PlaneT > pvi = pv.IntersectRay( start, dir, offsetcalc );
 	std::cout << start << " in direction " << dir << ( pv.Within( start ) ? ": starts inside " : ": starts outside " ) << pvi << std::endl;
+}
+
+template< int dim, class FieldT, class PlaneT, template< int, class > class OffsetCalcT >
+void checkIntersectionRay( const PlanarVolume< dim, FieldT, PlaneT > & pv, const vec3f & start, const vec3f & dir, const OffsetCalcT< dim, FieldT > & offsetcalc, FieldT startdist, const Matrix< dim, 1, FieldT > & startnormal, FieldT enddist, const Matrix< dim, 1, FieldT > & endnormal ) {
+	PlanarVolumeIntersection< dim, FieldT, PlaneT > pvi = pv.IntersectRay( start, dir, offsetcalc );
+	//std::cout << start << " in direction " << dir << ( pv.Within( start ) ? ": starts inside " : ": starts outside " ) << pvi << std::endl;
+	assert( pvi.enterfraction == startdist );
+	assert( !pvi.enterplane && !Norm( startnormal ) || pvi.enterplane && pvi.enterplane->GetNormal( ) == startnormal );
+	assert( pvi.exitfraction == enddist );
+	assert( !pvi.exitplane && !Norm( endnormal ) || pvi.exitplane && pvi.exitplane->GetNormal( ) == endnormal );
 }
 
 void test0( ) {
@@ -48,43 +78,60 @@ void test0( ) {
 		Plane< 3, float >( Vector3( 0.0f, 0.0f, -1.0f ), 1.0f ) };
 	PlanarVolume< 3, float, Plane< 3, float > > pv0( &p0[ 0 ], &p0[ 6 ] );
 
-	std::cout << "testing line segment intersections" << std::endl;
+	//std::cout << "testing line segment intersections" << std::endl;
 
 	// no intersection (outside)
 	// -inf ( 0 0 0 ) inf ( 0 0 0 )
-	printIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		//-std::numeric_limits< float >::infinity( ), vec3f( 0.0f ), std::numeric_limits< float >::infinity( ), vec3f( 0.0f ) );
+		1.0f, Vector3( -1.0f, 0.0f, 0.0f ), std::numeric_limits< float >::infinity( ), vec3f( 0.0f ) );
 
 	// front intersection
 	// 0.5 ( -1 0 0 ) inf ( 0 0 0 )
-	printIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		0.5f, Vector3( -1.0f, 0.0f, 0.0f ), std::numeric_limits< float >::infinity( ), vec3f( 0.0f ) );
 
 	// front and back intersection
 	// 0.25 ( -1 0 0 ) 0.75 ( 1 0 0 )
-	printIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 2.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 2.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionLine( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 2.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		0.25f, Vector3( -1.0f, 0.0f, 0.0f ), 0.75f, Vector3( 1.0f, 0.0f, 0.0f ) );
 
 	// back intersection
 	// -inf ( 0 0 0 ) 0.5 ( 1 0 0 )
-	printIntersectionLine( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 2.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 2.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionLine( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 2.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		-std::numeric_limits< float >::infinity( ), vec3f( 0.0f ), 0.5f, Vector3( 1.0f, 0.0f, 0.0f ) );
 
 	// no intersection (inside)
 	// -inf ( 0 0 0 ) inf ( 0 0 0 )
-	printIntersectionLine( pv0, Vector3( -0.5f, 0.0f, 0.0f ), Vector3( 0.5f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -0.5f, 0.0f, 0.0f ), Vector3( 0.5f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionLine( pv0, Vector3( -0.5f, 0.0f, 0.0f ), Vector3( 0.5f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		-std::numeric_limits< float >::infinity( ), vec3f( 0.0f ), std::numeric_limits< float >::infinity( ), vec3f( 0.0f ) );
 
 	// 
-	printIntersectionLine( pv0, Vector3( -1.5f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, -1.5f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -1.5f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, -1.5f ), ZeroOffsetCalc< 3, float >( ) );
+	//checkIntersectionLine( pv0, Vector3( -1.5f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, -1.5f ), ZeroOffsetCalc< 3, float >( ) );
 
 	// 
 	// -inf ( 0 0 0 ) inf ( 0 0 0 )
-	printIntersectionLine( pv0, Vector3( -2.5f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, -2.5f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -2.5f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, -2.5f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionLine( pv0, Vector3( -2.5f, 0.0f, 0.0f ), Vector3( 0.0f, 0.0f, -2.5f ), ZeroOffsetCalc< 3, float >( ),
+		-std::numeric_limits< float >::infinity( ), vec3f( 0.0f ), std::numeric_limits< float >::infinity( ), vec3f( 0.0f ) );
 
 	// corner case
-	printIntersectionLine( pv0, Vector3( -1.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -1.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//checkIntersectionLine( pv0, Vector3( -1.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
 
 	// corner case
-	printIntersectionLine( pv0, Vector3( -1.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.5f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( -1.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.5f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//checkIntersectionLine( pv0, Vector3( -1.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.5f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
 
 	// corner case
-	printIntersectionLine( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionLine( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//checkIntersectionLine( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
 }
 
 void test1( ) {
@@ -97,25 +144,33 @@ void test1( ) {
 		Plane< 3, float >( Vector3( 0.0f, 0.0f, -1.0f ), 1.0f ) };
 	PlanarVolume< 3, float, Plane< 3, float > > pv0( &p0[ 0 ], &p0[ 6 ] );
 
-	std::cout << "testing ray intersections" << std::endl;
+	//std::cout << "testing ray intersections" << std::endl;
 
 	// intersection (starts outside)
 	// 1 (-1 0 0) 3 (1 0 0)
-	printIntersectionRay( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionRay( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionRay( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		1.0f, Vector3( -1.0f, 0.0f, 0.0f ), 3.0f, Vector3( 1.0f, 0.0f, 0.0f ) );
 
 	// intersection (starts inside)
 	// -inf (0 0 0) 1 (1 0 0)
-	printIntersectionRay( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionRay( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionRay( pv0, Vector3( 0.0f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		-std::numeric_limits< float >::infinity( ), vec3f( 0.0f ), 1.0f, Vector3( 1.0f, 0.0f, 0.0f ) );
 
 	// no intersection
 	// -inf (0 0 0) inf (0 0 0)
-	printIntersectionRay( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionRay( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ) );
+	checkIntersectionRay( pv0, Vector3( -2.0f, 0.0f, 0.0f ), Vector3( -1.0f, 0.0f, 0.0f ), ZeroOffsetCalc< 3, float >( ),
+		-std::numeric_limits< float >::infinity( ), vec3f( 0.0f ), std::numeric_limits< float >::infinity( ), vec3f( 0.0f ) );
 
 	//
-	printIntersectionRay( pv0, Vector3( -2.5f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, -1.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionRay( pv0, Vector3( -2.5f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, -1.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//checkIntersectionRay( pv0, Vector3( -2.5f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, -1.0f ), ZeroOffsetCalc< 3, float >( ) );
 
 	//
-	printIntersectionRay( pv0, Vector3( -1.5f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, -1.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//printIntersectionRay( pv0, Vector3( -1.5f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, -1.0f ), ZeroOffsetCalc< 3, float >( ) );
+	//checkIntersectionRay( pv0, Vector3( -1.5f, 0.0f, 0.0f ), Vector3( 1.0f, 0.0f, -1.0f ), ZeroOffsetCalc< 3, float >( ) );
 }
 
 void test2( ) {
@@ -128,13 +183,13 @@ void test2( ) {
 		Plane< 3, float >( Vector3( 0.0f, 0.0f, -1.0f ), 1.0f ) };
 	PlanarVolume< 3, float, Plane< 3, float > > pv0( &p0[ 0 ], &p0[ 6 ] );
 
-	std::cout << "Generated points" << std::endl;
+	//std::cout << "Generated points" << std::endl;
 
 	std::list< vec3f > points = GeneratePoints( pv0 );
 
-	std::cout << "(";
-	std::copy( points.begin( ), points.end( ), std::ostream_iterator< vec3f >( std::cout, ") (" ) );
-	std::cout << std::endl;
+	//std::cout << "(";
+	//std::copy( points.begin( ), points.end( ), std::ostream_iterator< vec3f >( std::cout, ") (" ) );
+	//std::cout << std::endl;
 }
 
 int main( ) {
